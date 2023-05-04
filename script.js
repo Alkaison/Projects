bb8550fbe9fef76db62a58eb05dba10bc0be6559
Alkaison <505ganeshmourya@gmail.com>
2023-05-04 16:13:28 +0530

    Update: Improved javascript code for theme toggle

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const themeToggleBtn = document.getElementById("theme-icon");
 
 themeToggleBtn.addEventListener("click", () => {
-    if(themeToggleBtn.classList.contains("fa-sun"))
-    {
-        themeToggleBtn.classList.remove("fa-sun");
-        themeToggleBtn.classList.add("fa-moon");
-        document.body.classList.add("light-theme");
-    }
-    else
-    {
-        themeToggleBtn.classList.remove("fa-moon");
-        themeToggleBtn.classList.add("fa-sun");
-        document.body.classList.remove("light-theme");
-    }
+
+    themeToggleBtn.classList.toggle("fa-sun");
+    themeToggleBtn.classList.toggle("fa-moon");
+    document.body.classList.toggle("light-theme");
 });
